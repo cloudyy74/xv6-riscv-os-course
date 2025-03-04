e53d2893bf5d17d1c9ad21da0c8f8eef3bc6a0b7
Vitas <ccloudyy74@gmail.com>
2025-03-04 14:14:10 +0300

    fixed al

--- user/add.c
+++ user/add.c
@@ -9,61 +9,50 @@ main(int argc, char *argv[])
 {
   int i = 0, cc;
   char c;
+  char space = 0;
   char buf[BUF_SIZE];
+  char* sec;
   while (i + 1 < BUF_SIZE) {
     cc = read(0, &c, 1);
-    if (cc < 1) {
+    if (cc < 0) {
       fprintf(2, "add: read error\n");
-      exit(-1);
+      exit(1);
+    }
+    if (cc == 0 || c == '\n' || c == '\r') {
+      break;
     }
     if (c == ' ') {
-      break;
+      if (space++ > 0) {
+        fprintf(2, "add: wrong input format\n");
+        exit(1);
+      }
+      sec = buf + i + 1;
     }
-    if ('0' > c || '9' < c) {
+    else if (('0' > c || '9' < c) && c != ' ') {
       fprintf(2, "add: invalid character\n");
-      exit(-1);
+      exit(1);
     }
     buf[i++] = c;
   }
 
-  if (c != ' ') {
+  if (i + 1 == BUF_SIZE) {
     fprintf(2, "add: buffer overflow\n");
-    exit(-1);
+    exit(1);
   }
 
+  if (space == 0) {
+    fprintf(2, "add: wrong input format\n");
+    exit(1);
+  }
+  
   buf[i] = '\0';
 
-  printf("|%s ", buf);
+  printf("|%s|\n", buf);
 
   int a = atoi(buf);
-
-
-  i = 0;
-
-  while (i + 1 < BUF_SIZE) {
-    cc = read(0, &c, 1);
-    if (cc < 1) {
-      fprintf(2, "add: read error\n");
-      exit(-1);
-    }
-    if (c == '\n' || c == '\r') {
-      break;
-    }
-    if ('0' > c || '9' < c) {
-      fprintf(2, "add: invalid character\n");
-      exit(-1);
-    }
-    buf[i++] = c;
-  }
-  if (c != '\n' && c != '\r') {
-    fprintf(2, "add: buffer overflow\n");
-    exit(-1);
-  }
-  buf[i] = '\0';
-
-  printf("%s|\n", buf);
+  int b = atoi(sec);
   
-  int b = atoi(buf);
-
   printf("%d\n", a + b);
+  
+  exit(0);
 }
